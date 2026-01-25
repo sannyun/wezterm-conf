49783d5d5c5834d2964e1ca963497039817b837f
Aleksandr Iuniaev <sannyun@outlook.com>
2026-01-25 20:12:09 +0200

    feat: add custom tab formatting with colors and process icons

--- wezterm.lua
+++ wezterm.lua
@@ -7,8 +7,6 @@ wezterm.on("gui-startup", function(cmd)
 	gui_window:maximize()
 end)
 
--- local launch_menu = {}
-
 local config = wezterm.config_builder()
 
 config.window_decorations = "RESIZE"
@@ -33,40 +31,72 @@ config.line_height = 1.0
 config.underline_position = -3
 config.underline_thickness = 2
 
--- wezterm.on("update-status", function(window)
---
--- end)
+local process_icons = {}
+process_icons["nvim"] = ""
+process_icons["lazygit"] = ""
+process_icons["yazi"] = ""
+
+-- Convert arbitrary strings to a unique hex color value
+-- Based on: https://stackoverflow.com/a/3426956/3219667
+local function string_to_color(str)
+	-- Convert the string to a unique integer
+	local hash = 0
+	for i = 1, #str do
+		hash = string.byte(str, i) + ((hash << 5) - hash)
+	end
+	-- Convert the integer to a unique color
+	local hue = (hash & 0x1ff) / 512 * 360
+	local saturation = ((hash >> 9) & 255) / 255 * 60
+	local c = wezterm.color.from_hsla(hue, saturation, 0.18, 1)
+	return c
+end
+
+local function get_tab_color(tab_info)
+	local active_pane = tab_info.active_pane
+	local cwd = active_pane.current_working_dir
+	return string_to_color(cwd.file_path)
+end
+
+local function calculate_tab_title(tab_info)
+	local active_pane = tab_info.active_pane
+	local pane_title = active_pane.title
+	local title = pane_title
+
+	local process = active_pane.user_vars.WEZTERM_PROG
+	if process and #process > 0 then
+		local icon = process_icons[process]
+		if icon then
+			title = string.format("%s %s", icon, pane_title)
+		end
+	end
+
+	return title
+end
+
+local function get_tab_title(tab_info)
+	local title = tab_info.tab_title
+	if title and #title > 0 then
+		return title
+	end
+
+	return calculate_tab_title(tab_info)
+end
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-	local title = tab.active_pane.title
+	local title = get_tab_title(tab)
 
 	local color_scheme = config.resolved_palette
-	local bg = wezterm.color.parse(color_scheme.background)
 	local fg = wezterm.color.parse(color_scheme.foreground)
-	local cwd = tab.active_pane.current_working_dir
 
-	-- if tab.is_active then
-	-- 	return {
-	-- 		{ Background = { Color = "white" } },
-	-- 		{ Foreground = { Color = fg } },
-	-- 		{ Text = cwd },
-	-- 	}
-	-- end
+	if tab.is_active then
+		return {
+			{ Background = { Color = get_tab_color(tab) } },
+			{ Foreground = { Color = fg } },
+			{ Text = title },
+		}
+	end
+
+	return title
 end)
-
--- Return the tab's current working directory
-local function get_cwd(tab)
-	local pane = tab.active_pane
-	if not pane then
-		return ""
-	end
-
-	local cwd = pane.current_working_dir
-	if not cwd then
-		return ""
-	end
-
-	return cwd.file_path or ""
-end
 
 return config
